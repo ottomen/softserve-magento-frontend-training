c41a676acd9bf6a2372037e94a4f0b1e72c4329b
Arthur Shevchenko <ashev4@softserveinc.com>
2020-11-20 11:16:48 +0200

    add Contact from validation

---
+++ src/app/design/frontend/SoftServe/default/Magento_Contact/requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    map: {
+        "*": {
+            maskedinput: "Magento_Contact/js/jquery.maskedinput",
+        }
+    },
+    config: {
+        mixins: {
+            "mage/validation": {
+                "Magento_Contact/js/validation-mixin": true,
+            },
+        },
+    },
+};
