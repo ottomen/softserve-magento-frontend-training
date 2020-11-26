1352b7f46d86f07b14f28f8c576aefd2fc65862c
Arthur Shevchenko <ashev4@softserveinc.com>
2020-11-26 12:55:10 +0200

    add disabled states to increase/decrease pdp quantity

--- src/app/design/frontend/SoftServe/default/Magento_Catalog/web/js/increase-decrease-price.js
+++ src/app/design/frontend/SoftServe/default/Magento_Catalog/web/js/increase-decrease-price.js
@@ -1,48 +1,97 @@
+/**
+ * Copyright © SoftServe, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 define(["jquery"], function ($) {
+    "use strict";
     $.widget("softserve.increaseDecreasePrice", {
         options: {
             minValue: 1,
             maxValue: 10,
-            $inputElement: null,
-            $container: null,
+            inputElement: null,
+            container: null,
+            increaseMarkup:
+                '<span class="control__btn control__btn--plus">+</span>',
+            decreaseMarkup:
+                '<span class="control__btn control__btn--minus">-</span>',
+            inputWrapSelector: ".field.qty .control",
+            inputSelector: ".field.qty .control input",
+            increaseSelector: ".control__btn--plus",
+            decreaseSelector: ".control__btn--minus",
         },
+        /** @inheritdoc */
         _create: function () {
-            var $self = this;
-            this.options.$container = this.element;
-            this.options.$inputElement = this.element.find(
-                ".field.qty .control input"
+            var self = this;
+            this.options.container = this.element;
+            this.options.inputElement = this.element.find(
+                this.options.inputSelector
             );
             this._appendMarkup();
-            this.options.$container.on("click", ".control__btn--minus", function () {
-                $self._decrease();
-            });
-            this.options.$container.on("click", ".control__btn--plus", function () {
-                $self._increase();
-            });
+            this.options.container.on(
+                "click",
+                this.options.decreaseSelector,
+                function () {
+                    self._decrease();
+                }
+            );
+            this.options.container.on(
+                "click",
+                this.options.increaseSelector,
+                function () {
+                    self._increase();
+                }
+            );
         },
+        /**
+         * @private
+         */
         _appendMarkup: function () {
-            var $targetContainer = this.options.$container.find(
-                ".field.qty .control"
+            var $targetContainer = this.options.container.find(
+                this.options.inputWrapSelector
             );
-            $targetContainer.prepend(
-                '<span class="control__btn control__btn--minus">-</span>'
-            );
-            $targetContainer.append(
-                '<span class="control__btn control__btn--plus">+</span>'
-            );
-        },
-        _increase: function () {
-            var value = parseInt(this.options.$inputElement.val()) + 1;
-            if (value <= this.options.maxValue) {
-                this.options.$inputElement.val(value);
+            var value = parseInt(this.options.inputElement.val());
+            $targetContainer.prepend(this.options.decreaseMarkup);
+            $targetContainer.append(this.options.increaseMarkup);
+            if (value <= this.options.minValue) {
+                $(this.options.decreaseSelector).addClass("disabled");
+            } else if (value >= this.options.maxValue) {
+                $(this.options.increaseSelector).addClass("disabled");
             }
         },
+        /**
+         * @private
+         */
+        _increase: function () {
+            var value = parseInt(this.options.inputElement.val()) + 1;
+            if (value < this.options.maxValue) {
+                this.options.inputElement.val(value);
+                $(this.options.increaseSelector).removeClass("disabled");
+                $(this.options.decreaseSelector).removeClass("disabled");
+            } else if (value === this.options.maxValue) {
+                this.options.inputElement.val(value);
+                $(this.options.increaseSelector).addClass("disabled");
+            } else {
+                $(this.options.increaseSelector).addClass("disabled");
+            }
+        },
+        /**
+         * @private
+         */
         _decrease: function () {
-            var value = parseInt(this.options.$inputElement.val()) - 1;
-            if (value >= this.options.minValue) {
-                this.options.$inputElement.val(value);
+            var value = parseInt(this.options.inputElement.val()) - 1;
+            if (value > this.options.minValue) {
+                this.options.inputElement.val(value);
+                $(this.options.decreaseSelector).removeClass("disabled");
+                $(this.options.increaseSelector).removeClass("disabled");
+            } else if (value === this.options.minValue) {
+                this.options.inputElement.val(value);
+                $(this.options.decreaseSelector).addClass("disabled");
+            } else {
+                $(this.options.decreaseSelector).addClass("disabled");
             }
         },
     });
+
     return $.softserve.increaseDecreasePrice;
 });
